e582585b36c89a13e509f87ce829e642aaa931dc
nnmngator <45370339+nnmngator@users.noreply.github.com>
2018-11-26 23:06:54 +0100

    Add files via upload

---
+++ xD.h
@@ -0,0 +1,16 @@
+#ifndef__xDD__
+#define __xDD__ 
+
+#define xd ;
+#define xD ;
+#define XD ;
+#define xdd ;
+#define xDD ;
+#define XDD ;
+#define xddd ;
+#define xDDD ;
+#define XDDD ;
+
+
+
+#endif
